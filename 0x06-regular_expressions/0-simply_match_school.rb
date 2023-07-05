a79f4f7f089a99da53027d969943e443210bb7f9
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-07-04 19:31:08 -0700

    regex match school

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+input = ARGV[0]
+
+if input =~ /School/
+  puts "School"
+else
+  puts ""
+end
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,2 @@
+#0x06-regular_expressions
+This directory contains the commands for the REGEXP project.
